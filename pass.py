95204506709e4869e372f837387c0ad801afba3e
physics-grunt <59495064+physics-grunt@users.noreply.github.com>
2020-01-03 22:13:17 -0600

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,19 @@
+import random
+
+
+N=16
+l = []
+s=''
+for i in range(33,95):
+	l+= chr(i)
+random.shuffle(l)   
+for i in range(97,127):
+    l+= chr(i)
+random.shuffle(l)
+l*=N
+random.shuffle(l)
+for i in range(N):
+    s += l[i]
+random.shuffle(l)
+
+print(s[:N])
